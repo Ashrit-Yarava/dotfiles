e6fc966710bc6b39a554ae46f1641eaf9d6e5348
Ashrit Yarava <ashdragon95@gmail.com>
2020-11-22 12:10:38 -0500

    Update Fish Config, Kakrc

---
+++ fish/config.fish
@@ -0,0 +1,17 @@
+# Application Links
+alias typora="open -a Typora"
+alias subl="open -a Sublime\ Text"
+alias code="open -a Visual\ Studio\ Code"
+alias open-pdf="open -a Preview.app"
+alias neovide="open -a Neovide"
+
+# Environment Config
+function activate-ai
+    source ~/Desktop/Programming/Machine\ Learning/pyai/bin/activate.fish
+    cd ~/Desktop/Programming/Machine\ Learning/
+end
+
+function activate-hacking
+    source "/Users/ashrityarava/Documents/hacking/source-file.fish"
+    cd ~/Desktop/Programming/CTFs/
+end
